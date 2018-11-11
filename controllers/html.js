3d8304572d6b27fba9c16f667c7ccbea3dffe2b6
unknown <jtc7537@gmail.com>
2018-11-11 02:11:26 -0500

    bio page

--- controllers/html.js
+++ controllers/html.js
@@ -3,5 +3,8 @@ const path = require('path')
 module.exports = app => {
     app.get('/', (req,res) => {
         res.sendFile(path.join(__dirname,'./../public/html/index.html'))
+    });
+    app.get('/about', (req,res) => {
+        res.sendFile(path.join(__dirname,'./../public/html/about.html'))
     })
 }
